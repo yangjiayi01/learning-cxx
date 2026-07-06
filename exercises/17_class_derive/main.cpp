15f796b63636e45b03d68a1acd82e24a08a1727f
yangjiayi01 <106996809+yangjiayi01@users.noreply.github.com>
2026-07-06 11:47:32 +0800

    Complete exercise exercises/17_class_derive/main.cpp

--- exercises/17_class_derive/main.cpp
+++ exercises/17_class_derive/main.cpp
@@ -37,7 +37,7 @@ struct B : public A {
         std::cout << ++i << ". " << "B(" << a << ", X(" << x.x << "))" << std::endl;
     }
     B(B const &other) : A(other.a), x(other.x) {
-        std::cout << ++i << ". " << "B(B const &) : A(" << a << "), x(X(" << x.x << "))" << std::endl;
+        std::cout << ++i << ". " << "B(B const &) : A(" << a << "), X(X(" << x.x << "))" << std::endl;
     }
     ~B() {
         std::cout << ++i << ". " << "~B(" << a << ", X(" << x.x << "))" << std::endl;
@@ -50,9 +50,9 @@ int main(int argc, char **argv) {
     B b = B(3);
 
     // TODO: 补全三个类型的大小
-    static_assert(sizeof(X) == ?, "There is an int in X");
-    static_assert(sizeof(A) == ?, "There is an int in A");
-    static_assert(sizeof(B) == ?, "B is an A with an X");
+    static_assert(sizeof(X) == 4, "There is an int in X");
+    static_assert(sizeof(A) == 4, "There is an int in A");
+    static_assert(sizeof(B) == 8, "B is an A with an X");
 
     i = 0;
     std::cout << std::endl
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
     // 这也是不可能的，因为 A 是 B 的一部分，就好像不可以把套娃的外层放进内层里。
     A ab = B(5);// 然而这个代码可以编译和运行！
     // THINK: 观察打印出的信息，推测把大象放进冰箱分几步？
-    // THINK: 这样的代码是“安全”的吗？
+    // THINK: 这样的代码是"安全"的吗？
     // NOTICE: 真实场景中不太可能出现这样的代码
 
     i = 0;
