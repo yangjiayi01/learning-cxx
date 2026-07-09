4c7c21de2dedc9d10062508fa2a830917a4af4ab
yangjiayi01 <106996809+yangjiayi01@users.noreply.github.com>
2026-07-09 14:40:33 +0800

    Complete all 34 C++ exercises (34/34)

---
+++ exercises\05_constexpr\main.cpp
@@ -0,0 +1,25 @@
+#include "../exercise.h"
+
+// 带缓存的斐波那契计算
+constexpr unsigned long long fibonacci(int i) {
+    unsigned long long cache[100]{0, 1};
+    if (i < 2) return cache[i];
+    for (int n = 2; n <= i; ++n) {
+        cache[n] = cache[n-1] + cache[n-2];
+    }
+    return cache[i];
+}
+
+int main(int argc, char **argv) {
+    constexpr auto FIB20 = fibonacci(20);
+    ASSERT(FIB20 == 6765, "fibonacci(20) should be 6765");
+    std::cout << "fibonacci(20) = " << FIB20 << std::endl;
+
+    // TODO: 观察错误信息，修改一处，使代码编译运行
+    // PS: 编译运行，但是不一定能算出结果……
+    constexpr auto ANS_N = 90;
+    constexpr auto ANS = fibonacci(90);
+    std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
+
+    return 0;
+}
